8b325b30d4143b26396a2c32963b0cee6128b469
bkleinh <74694427+bkleinh@users.noreply.github.com>
2024-09-16 19:10:31 -0400

    Add missing file

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/types/HydraObjectLocations.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode.types;
+
+/**
+ * Potential object locations to use with object detection
+ */
+public enum HydraObjectLocations {
+    ObjLocUnknown,
+    ObjLocBlueLeftSpike,
+    ObjLocBlueCenterSpike,
+    ObjLocBlueRightSpike,
+    ObjLocRedLeftSpike,
+    ObjLocRedCenterSpike,
+    ObjLocRedRightSpike
+}
