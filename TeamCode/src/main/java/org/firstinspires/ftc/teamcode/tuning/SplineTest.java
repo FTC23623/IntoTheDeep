8598ab5b89b0a791a09445e07e5d29880e0ea913
bkleinh <74694427+bkleinh@users.noreply.github.com>
2024-10-19 12:42:30 -0400

    Replace spline to test auton

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/SplineTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/SplineTest.java
@@ -11,7 +11,7 @@ import org.firstinspires.ftc.teamcode.TankDrive;
 public final class SplineTest extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
-        Pose2d beginPose = new Pose2d(0, 0, 0);
+        Pose2d beginPose = new Pose2d(30, 63, HeadingRad(-90));
         if (TuningOpModes.DRIVE_CLASS.equals(MecanumDrive.class)) {
             MecanumDrive drive = new MecanumDrive(hardwareMap, beginPose);
 
@@ -19,8 +19,17 @@ public final class SplineTest extends LinearOpMode {
 
             Actions.runBlocking(
                 drive.actionBuilder(beginPose)
-                        .splineTo(new Vector2d(30, 30), Math.PI / 2)
-                        .splineTo(new Vector2d(0, 60), Math.PI)
+                        .splineToLinearHeading(new Pose2d(54,54, HeadingRad(-135)),HeadingRad(0))
+                        .splineToLinearHeading(new Pose2d(59,46,HeadingRad(-90)),HeadingRad(-90))
+                        .splineToLinearHeading(new Pose2d(54,54, HeadingRad(-135)),HeadingRad(0))
+                        .splineToLinearHeading(new Pose2d(49,46, HeadingRad(-90)),HeadingRad(-90))
+                        .splineToLinearHeading(new Pose2d(54,54, HeadingRad(-135)),HeadingRad(0))
+                        .splineToLinearHeading(new Pose2d(51,26,HeadingRad(0)),HeadingRad(90))
+                        .splineToLinearHeading(new Pose2d(54,54, HeadingRad(-135)),HeadingRad(0))
+                        // .lineTo(new Pose2d(56,15, HeadingRad(180)), HeadingRad(180))
+                        // .splineToLinearHeading(new Pose2d(38,10,HeadingRad(180)),HeadingRad(-90))
+                        .splineToLinearHeading(new Pose2d(46,54, HeadingRad(-90)), HeadingRad(0))
+                        .splineToLinearHeading(new Pose2d(25,10,HeadingRad(180)),HeadingRad(180))
                         .build());
         } else if (TuningOpModes.DRIVE_CLASS.equals(TankDrive.class)) {
             TankDrive drive = new TankDrive(hardwareMap, beginPose);
@@ -35,5 +44,9 @@ public final class SplineTest extends LinearOpMode {
         } else {
             throw new RuntimeException();
         }
+    }
+
+    public static double HeadingRad(double degrees) {
+        return Math.toRadians(degrees);
     }
 }
