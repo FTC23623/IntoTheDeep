0cdc53088599fe560722937a8d26a3643f332e92
bkleinh <74694427+bkleinh@users.noreply.github.com>
2024-10-20 16:48:31 -0400

    Start working on intake control

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/types/HydraConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/types/HydraConstants.java
@@ -6,4 +6,7 @@ public final class HydraConstants {
     public static final double driveBoosted = 1;
     public static final double driveNormal = 0.9;
     public static final double driveSlow = 0.5;
+    public static final double contServoOff = 0.5;
+    public static final double contServoForward = 1.0;
+    public static final double contServoBackward = 0.0;
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/types/IntakeStates.java
@@ -0,0 +1,5 @@
+package org.firstinspires.ftc.teamcode.types;
+
+public enum IntakeStates {
+    IntakeSt_Idle, IntakeSt_In, IntakeSt_InDetected,  IntakeSt_Hold, IntakeSt_Out
+}
