7ccdb10c533cd86124a771c4091272751192c437
bkleinh <74694427+bkleinh@users.noreply.github.com>
2024-10-27 21:19:55 -0400

    Make our own spline test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/SplineTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/SplineTest.java
@@ -11,33 +11,17 @@ import org.firstinspires.ftc.teamcode.TankDrive;
 public final class SplineTest extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
-        Pose2d beginPose = new Pose2d(30, 63, HeadingRad(-90));
+        Pose2d beginPose = new Pose2d(0, 0, 0);
         if (TuningOpModes.DRIVE_CLASS.equals(MecanumDrive.class)) {
             MecanumDrive drive = new MecanumDrive(hardwareMap, beginPose);
 
             waitForStart();
-            if (true) {
-                Actions.runBlocking(
-                        drive.actionBuilder(beginPose)
-                                .splineToLinearHeading(new Pose2d(54, 54, HeadingRad(-135)), HeadingRad(0))
-                                .splineToLinearHeading(new Pose2d(59, 46, HeadingRad(-90)), HeadingRad(-90))
-                                .splineToLinearHeading(new Pose2d(54, 54, HeadingRad(-135)), HeadingRad(0))
-                                .splineToLinearHeading(new Pose2d(49, 46, HeadingRad(-90)), HeadingRad(-90))
-                                .splineToLinearHeading(new Pose2d(54, 54, HeadingRad(-135)), HeadingRad(0))
-                                .splineToLinearHeading(new Pose2d(51, 26, HeadingRad(0)), HeadingRad(90))
-                                .splineToLinearHeading(new Pose2d(54, 54, HeadingRad(-135)), HeadingRad(0))
-                                // .lineTo(new Pose2d(56,15, HeadingRad(180)), HeadingRad(180))
-                                // .splineToLinearHeading(new Pose2d(38,10,HeadingRad(180)),HeadingRad(-90))
-                                .splineToLinearHeading(new Pose2d(46, 54, HeadingRad(-90)), HeadingRad(0))
-                                .splineToLinearHeading(new Pose2d(25, 10, HeadingRad(180)), HeadingRad(180))
-                                .build());
-            } else {
-                Actions.runBlocking(
-                        drive.actionBuilder(beginPose)
-                                .lineToY(-17)
-                                .build());
 
-            }
+            Actions.runBlocking(
+                drive.actionBuilder(beginPose)
+                        .splineTo(new Vector2d(30, 30), Math.PI / 2)
+                        .splineTo(new Vector2d(0, 60), Math.PI)
+                        .build());
         } else if (TuningOpModes.DRIVE_CLASS.equals(TankDrive.class)) {
             TankDrive drive = new TankDrive(hardwareMap, beginPose);
 
@@ -51,9 +35,5 @@ public final class SplineTest extends LinearOpMode {
         } else {
             throw new RuntimeException();
         }
-    }
-
-    public static double HeadingRad(double degrees) {
-        return Math.toRadians(degrees);
     }
 }
