2dc07ee9f90c83a587b64f95437607ca7736f9ca
bkleinh <74694427+bkleinh@users.noreply.github.com>
2024-11-14 19:18:18 -0500

    Specimen arm PID

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/SpecimenArm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/SpecimenArm.java
@@ -1,8 +1,10 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
 import com.acmerobotics.dashboard.config.Config;
+import com.arcrobotics.ftclib.controller.PIDController;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 import org.firstinspires.ftc.teamcode.objects.HydraOpMode;
 
@@ -10,27 +12,44 @@ import org.firstinspires.ftc.teamcode.objects.HydraOpMode;
 public class SpecimenArm {
     private HydraOpMode mOp;
     private final DcMotorEx mLiftMotor;
-    private double mLiftTargetDeg;
+    private final PIDController mSpecArmPid;
+    // PID coefficients
+    public static double mSpecArmP = 0.022;
+    public static double mSpecArmI = 0.0;
+    public static double mSpecArmD = 0.0018;
+    // Target in degrees
+    private double mLiftTargetTicks;
+    // REV Core Hex
+    // 288 ticks per rotation
     private final double mTicksPerDegree = 288.0 / 360.0;
+    public static double mStartAngle = 8.0;
+    // under at 117
+    // score run to 35
     
     public SpecimenArm(HydraOpMode opMode) {
         mOp = opMode;
         mLiftMotor = mOp.mHardwareMap.get(DcMotorEx.class, "specArmMotor");
-        mLiftTargetDeg = 0;
+        mSpecArmPid = new PIDController(mSpecArmP, mSpecArmI, mSpecArmD);
+        mLiftTargetTicks = 0;
         mLiftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         mLiftMotor.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        mLiftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
     }
 
     public void SetAngle(double angle) {
-        mLiftTargetDeg = angle;
+        mLiftTargetTicks = (angle - mStartAngle) * mTicksPerDegree;
     }
 
     public void Process() {
-        mLiftMotor.setTargetPosition((int)(mLiftTargetDeg * mTicksPerDegree));
-        mLiftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        mLiftMotor.setPower(1.0);
-        mOp.mTelemetry.addData("A", mLiftTargetDeg * mTicksPerDegree);
-        mOp.mTelemetry.addData("A Tgt", mLiftMotor.getTargetPosition());
+        // get the current position to calculate error
+        int currentPos = mLiftMotor.getCurrentPosition();
+        mSpecArmPid.setPID(mSpecArmP, mSpecArmI, mSpecArmD);
+        // calculate pid for power and run
+        double pid = mSpecArmPid.calculate(currentPos, mLiftTargetTicks);
+        mLiftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        mLiftMotor.setPower(pid);
+        mOp.mTelemetry.addData("A Tgt", mLiftTargetTicks);
         mOp.mTelemetry.addData("A Pos", mLiftMotor.getCurrentPosition());
+        mOp.mTelemetry.addData("A Pwr", pid);
     }
 }
